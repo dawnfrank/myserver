3e3036c58e13277c0f1423c2878b6a6a82fb302a
tianzigeng <250445881@qq.com>
2018-08-05 14:46:13 +0800

    1
    1

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
     app.run()
