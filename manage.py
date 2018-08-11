1d34bdaf840a3a98a537d7647c6c6dc9d0b99f8d
tianzigeng <250445881@qq.com>
2018-08-11 23:02:50 +0800

    1
    1

--- manage.py
+++ manage.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from flask import render_template
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
