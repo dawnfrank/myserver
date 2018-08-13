4fb581679497268c076112e18806242b32d4f498
tianzigeng <250445881@qq.com>
2018-08-13 20:49:59 +0800

    1
    1

--- manage.py
+++ manage.py
@@ -3,12 +3,15 @@
 
 from flask import render_template
 from flask import Flask
+from flask import globals
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/hello')
+@app.route('/hello/<name>')
+def index(name=None):
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run()
